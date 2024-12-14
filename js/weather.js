470e7321814c6e7806427e93fd7699d33a55afe9
Joa-web <condoly5@gmail.com>
2024-12-14 13:18:40 +0900

    done

--- js/weather.js
+++ js/weather.js
@@ -9,12 +9,27 @@ function onGeoOk (position) {
         const weather = document.querySelector('#weatherBox span:first-child')
         const temp = document.querySelector('#weatherBox span:nth-child(2)')
         const city = document.querySelector('#weatherBox span:last-child')
+        const weatherChange = data.weather[0].main
         const icon = data.weather[0].icon
         const tempNum = data.main.temp
-        const iconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`
-        weather.innerHTML = `<image src='${iconUrl}' width=50px>${data.weather[0].main}`
+        if (weatherChange == 'Clear') {
+            weather.innerHTML=`<i class="fa-solid fa-sun fa-lg"></i>${weatherChange}`
+        } else if(weatherChange == 'Cloud') {
+            weather.innerHTML=`<i class="fa-solid fa-cloud fa-lg"></i>${weatherChange}`
+        } else if(weatherChange =='Thunderstorm') {
+            weather.innerHTML=`<i class="fa-solid fa-cloud-bolt fa-lg"></i>${weatherChange}`
+        } else if(weatherChange == 'Drizzle') {
+            weather.innerHTML=`<i class="fa-solid fa-cloud-rain fa-lg"></i>${weatherChange}`
+        } else if(weatherChange == 'Rain' ) {
+            weather.innerHTML=`<i class="fa-solid fa-cloud-showers-heavy fa-lg"></i>${weatherChange}`
+        } else if (weatherChange == 'Snow') {
+            weather.innerHTML=`<i class="fa-solid fa-snowflake fa-lg"></i>${weatherChange}`
+        } else if (weatherChange == 'Atmoshere') {
+            weather.innerHTML=`<i class="fa-brands fa-soundcloud fa-lg"></i>${weatherChange}`
+        }
         temp.innerText = `${tempNum.toFixed(1)} °C`
         city.innerText = data.name
+        
     });
 }
 function onGeoError() {
