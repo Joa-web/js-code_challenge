515649f23ac38a12ad42334993bb4a86503c5f5a
Joa-web <condoly5@gmail.com>
2024-12-14 16:03:55 +0900

    changeBg

--- js/bg.js
+++ js/bg.js
@@ -1,5 +1,4 @@
-const img = ['00.jpeg', '01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg', '06.webp',
-    '07.jpg', '08.jpg']
+const img = ['00.jpg', '01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg', '06.jpg']
 const randomImg = img[parseInt(Math.random() * img.length)]
 document.body.style.background = `url(img/${randomImg}) no-repeat`
 document.body.style.backgroundSize = 'cover'
